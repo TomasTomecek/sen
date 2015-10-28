83d5f110645e7bbc2758cdd4383aa1485bed3d00
Tomas Tomecek <ttomecek@redhat.com>
2015-10-28 21:50:16 +0100

    logging: nicer logs

--- sen/__init__.py
+++ sen/__init__.py
@@ -15,6 +15,6 @@ def set_logging(name="sen", level=logging.DEBUG):
         handler = logging.StreamHandler(sys.stderr)
 
     handler.setLevel(logging.DEBUG)
-    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    formatter = logging.Formatter('%(asctime)s %(name)-18s %(levelname)-6s %(message)s', '%H:%M:%S')
     handler.setFormatter(formatter)
     logger.addHandler(handler)
