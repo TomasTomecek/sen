3713419c6f4d5e742bce6855981f40119e119a47
Tomas Tomecek <ttomecek@redhat.com>
2016-01-02 13:00:47 +0100

    AdHocAttrMap: support multiple markups with urwid.Text

--- sen/tui/widgets/util.py
+++ sen/tui/widgets/util.py
@@ -1,4 +1,10 @@
+import logging
+
 import urwid
+
+from sen.tui.constants import MAIN_LIST_FOCUS
+
+logger = logging.getLogger(__name__)
 
 
 class AdHocAttrMap(urwid.AttrMap):
@@ -10,7 +16,18 @@ class AdHocAttrMap(urwid.AttrMap):
     def __init__(self, w, maps, init_map='normal'):
         self.maps = maps
         urwid.AttrMap.__init__(self, w, maps[init_map])
+        if isinstance(w, urwid.Text):
+            self.attrs = [x[0] for x in self.original_widget.get_text()[1]]
 
     def set_map(self, attrstring):
-        self.set_attr_map({None: self.maps[attrstring]})
+        attr_map = {None: self.maps[attrstring]}
 
+        # for urwid.Text only: do hovering for all markups in the widget
+        if isinstance(self.original_widget, urwid.Text):
+            if attrstring == "normal":
+                for a in self.attrs:
+                    attr_map[self.maps["focus"]] = a
+            elif attrstring == "focus":
+                for a in self.attrs:
+                    attr_map[a] = self.maps["focus"]
+        self.set_attr_map(attr_map)
