a5e9dce1e1e7ac21f180d36a20612b95c84dc1e9
Tomas Tomecek <ttomecek@redhat.com>
2016-01-04 22:58:17 +0100

    tests: add containers(), improve images()

--- tests/real.py
+++ tests/real.py
@@ -2,17 +2,33 @@ from flexmock import flexmock
 import docker
 
 
+# docker 1.9
 image_data = {
     'Created': 1414577076,
     'Id': '3ab9a7ed8a169ab89b09fb3e12a14a390d3c662703b65b4541c0c7bde0ee97eb',
     'ParentId': 'a79ad4dac406fcf85b9c7315fe08de5b620c1f7a12f45c8185c843f4b4a49c4e',
+    'RepoDigests': [],
     'RepoTags': ['image:latest'],
     'Size': 0,
     'VirtualSize': 856564160
 }
 
+container_data = {
+    'Command': 'ls',
+    'Created': 1451419101,
+    'HostConfig': {'NetworkMode': 'default'},
+    'Id': 'ce3779014be349654fb757d6eed61954fb33b75510a3845e0dd8107ed8bef765',
+    'Image': 'banana',
+    'ImageID': '0a31ac8bd1d47354c824984916e298726fe2525ba19f07da7865004b256588a3',
+    'Labels': {'aqwe': 'zxcasd', 'aqwe2': 'zxcasd2', 'x': 'y'},
+    'Names': ['/elated_bose'],
+    'Ports': [],
+    'Status': 'Exited (0) 47 hours ago'
+}
 
-version_data = {'ApiVersion': '1.21',
+
+version_data = {
+    'ApiVersion': '1.21',
     'Arch': 'amd64',
     'BuildTime': 'Thu Sep 10 17:53:19 UTC 2015',
     'GitCommit': 'af9b534-dirty',
@@ -28,6 +44,12 @@ def images_response(*args, **kwargs):
     return [image_data]
 
 
+def containers_response(*args, **kwargs):
+    global container_data
+    return [container_data]
+
+
 def mock():
     flexmock(docker.Client, images=images_response)
+    flexmock(docker.Client, containers=containers_response)
     flexmock(docker.Client, version=lambda *args, **kwargs: version_data)
--- tests/test_docker_backend.py
+++ tests/test_docker_backend.py
@@ -1,5 +1,5 @@
 from sen.docker_backend import DockerBackend
-from .real import image_data, mock
+from .real import image_data, mock, container_data
 
 
 def test_images_call():
@@ -10,3 +10,23 @@ def test_images_call():
     assert len(images_response) == 1
     assert images_response[0].image_id == image_data["Id"]
     assert images_response[0].short_name == image_data["RepoTags"][0]
+    assert images_response[0].parent_id == image_data["ParentId"]
+    assert images_response[0].created_int == image_data["Created"]
+    assert [str(x) for x in images_response[0].names] == image_data["RepoTags"]
+
+
+def test_containers_call():
+    mock()
+    b = DockerBackend()
+    operation = b.get_containers()
+    containers_response = operation.response
+    assert len(containers_response) == 1
+    assert containers_response[0].container_id == container_data["Id"]
+    assert containers_response[0].names == container_data["Names"]
+    assert containers_response[0].short_name == container_data["Names"][0]
+    assert containers_response[0].created_int == container_data["Created"]
+    assert containers_response[0].command == container_data["Command"]
+    assert containers_response[0].status == container_data["Status"]
+    assert containers_response[0].running is False
+    assert containers_response[0].image_id == container_data["ImageID"]
+    # assert containers_response[0].image_name()
