8931f8e3680cd970a2c49629a08ee5ddb4132d3b
Tomas Tomecek <ttomecek@redhat.com>
2016-03-12 11:49:30 +0100

    improve logging
     * display msecs
     * show only filename

--- sen/__init__.py
+++ sen/__init__.py
@@ -13,7 +13,7 @@ def set_logging(name="sen", level=logging.DEBUG, path=FALLBACK_LOG_PATH):
     handler.setLevel(logging.DEBUG)
     # handler = logging.StreamHandler(sys.stderr)
 
-    formatter = logging.Formatter('%(asctime)s %(name)-18s %(levelname)-6s %(message)s',
-                                  '%H:%M:%S')
+    formatter = logging.Formatter(
+        '%(asctime)s.%(msecs).03d %(filename)-17s %(levelname)-6s %(message)s', '%H:%M:%S')
     handler.setFormatter(formatter)
     logger.addHandler(handler)
